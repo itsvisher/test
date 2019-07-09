92abff1b07f8caa26b0e145f0b4c00058189fcec
Vishal Srivastava <vishal@quantela.com>
2019-07-10 01:55:33 +0530

    Added /info GET API.

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const port = 8000
 
 app.get('/', (req, res) => res.send('Hello World! Automated release successful.'))
 
+app.get('/info', (req, res) => {
+		var js = JSON.parse('{"version": "v1.8.0"}');
+		res.send(js);
+	}
+);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
